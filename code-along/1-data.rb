94ae65d47035de099e775475da19b26643019d75
snuslein <sarah.nuslein@kellogg.northwestern.edu>
2023-01-24 20:48:46 +0000

    Ruby Intro

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,47 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5
+puts 2
 
 # Perform simple math with numbers
 
+puts 5 - 2
+puts 5 + 2
+puts 5 * 2
+puts 5 / 2
+# puts 5 % 2
+
+puts 5.0 / 2.0
+puts 5 * (2 + 1)
+
 # Strings
+
+puts "Hello, world!" 
 
 # Combine strings together
 
+puts "Tacos are" + "delish"
+puts "Tacos" * 3
+puts "Tacos:" + 3.to_s
+
 # Variables
+food = "Tacos"
+quantity = 3
+puts food * quantity
 
 # Combine strings and variables
 
+first_name = "Grogu"
+puts "Hello, " + first_name + "!"
+puts "Hello, #{first_name}!"
+greeting  = "Hello, #{first_name}!"
+puts greeting
+
+puts "#{food}: #{quantity}"
+
 # String manipulation
+
+puts first_name.upcase
+puts greeting.reverse
+puts greeting.swapcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,63 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+# is_true = true
+# is_false = false
+# puts is_true
+# puts is_false
+
 
 # Boolean Expressions
+puts 3 > 2
+puts 3 < 2
+puts 3 == 2
+# x = 2
+puts 3 != 2
 
 # If Conditional Logic
+if 3 > 2 
+    #....
+    puts "awesome"
+    puts "math works!"
+end
+
+if 3 < 2
+    puts "What?!"
+ends
 
 # If/Else Conditional Logic
 
+if 3 > 2 
+    puts "awesome"
+    puts "math works!"
+else
+    puts "WHAT"
+end
+
+password = "Tacos"
+user_entered_password = "password1"
+if password == user_entered_password
+    puts "you're in"
+else
+    puts "go away"
+end
+
 # Elsif Conditional Logic
 
+home_team_score = 101
+away_team_score = 101
+elsif home_team_score > away_team_score
+    puts "winner!"
+elsif home_team_score == away_team_score
+    puts "tiedd"
+else
+    puts "loser"
+end
+
 # Combining Expressions
+temp = 68
+if temp < 80 && temp > 60
+    puts "it's beautiful"
+else 
+    puts "brrrr"
+end 
